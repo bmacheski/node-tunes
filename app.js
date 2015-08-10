0f4afd74d38c26636aa9703e418ff1127ff6b4fe
bmacheski <bwm0007@gmail.com>
2015-08-10 15:32:39 -0500

    create mongodb form persistence

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var express = require('express')
-  , app     = express();
+var express    = require('express')
+  , app        = express()
+  , bodyParser = require('body-parser');
+
+require('./lib/mongodb');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('templates/artists');
+})
+
+app.get('/newartist', function (req, res) {
+  res.render('templates/newartist')
+})
+
+app.post('/artist/create', function (req, res){
+  var collection = global.db.collection('artists');
+
+  collection.save(req.body, function () {
+    res.redirect('/')
+  });
 })
 
 var port = process.env.PORT || 3000;
